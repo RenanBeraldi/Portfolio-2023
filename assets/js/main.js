2caa6a8765571f5c3c95ee233a6490580e5c4806
Renan Beraldi Castiglioni <renanberaldi@outlook.com>
2023-11-06 12:15:36 -0300

    Revealing Elements On Scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,46 @@ window.addEventListener("scroll", () => {
     nav.classList.remove("nav__show");
   }
 });
+
+// Revealing Elements on Scroll
+ScrollReveal().reveal(".primary__heading", {
+  delay: 500,
+  origin: "bottom",
+  distance: "40px",
+  reset: true,
+});
+
+ScrollReveal().reveal(".about__left", {
+  delay: 600,
+  origin: "right",
+  distance: "20px",
+  reset: true,
+});
+
+ScrollReveal().reveal(".about__right", {
+  delay: 600,
+  origin: "left",
+  distance: "20px",
+  reset: true,
+});
+
+ScrollReveal().reveal(".skills__line-1", {
+  delay: 500,
+  origin: "left",
+  distance: "40px",
+  reset: true,
+});
+
+ScrollReveal().reveal(".skills__line-2", {
+  delay: 600,
+  origin: "right",
+  distance: "40px",
+  reset: true,
+});
+
+ScrollReveal().reveal(".contact__content, .contact__header", {
+  delay: 700,
+  origin: "bottom",
+  distance: "40px",
+  reset: true,
+});
