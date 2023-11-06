b0e68118865b5e690edd8da07163cfa5e08ccd9c
Renan Beraldi Castiglioni <renanberaldi@outlook.com>
2023-11-06 00:16:49 -0300

    Desktop version finished!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
 "use strict";
+
+window.addEventListener("scroll", () => {
+  const nav = document.querySelector(".nav");
+  const sectionAbout = document.querySelector("#about");
+
+  if (window.scrollY >= sectionAbout.getBoundingClientRect().top) {
+    nav.classList.add("sticky");
+    nav.classList.remove("nav__show");
+  } else {
+    nav.classList.remove("sticky");
+    nav.classList.add("nav__show");
+  }
+
+  if (window.scrollY === 0) {
+    nav.classList.remove("nav__show");
+  }
+});
